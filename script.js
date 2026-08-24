f975e8c1412af0803ea114cdaab50e8a05159ba8
gabrielmoreiraconceicao-cmyk <gabriel.moreira.conceicao@escola.pr.gov.br>
2026-08-24 20:44:10 -0300

    .

--- script.js
+++ script.js
@@ -15,3 +15,16 @@
             }
         }
     })
+
+const btnTemaEscuro = document.querySelectorAll(".btn-Tema-escuro");
+
+btnTemaEscuro.dEventListener("click" , mudaTema);
+function mudaTema() {
+   console corpoPagina = document.body;
+   if (corpoPagina.classList.contains("tema-escuro")) {
+       corpoPagina.classList.removi("tema-escuro");
+   } else {
+    corpoPagina.classList.add("tema-escuro");
+   }
+    
+   }
